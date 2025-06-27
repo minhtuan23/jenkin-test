4f58f2fa1e73f8e168b8c98f095a6a5356fe93a2
minhtuan23 <hoangminh2306602@gmail.com>
2025-06-27 10:19:14 +0700

    push code

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
-const grep = require("cypress-grep/src/plugin"); 
+const grep = require("cypress-grep/src/plugin");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      grep(config); 
+      grep(config);
       return config;
     },
     specPattern: "cypress/e2e/**/*.cy.{js,ts}",
